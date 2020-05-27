4d53abb008473efb913a0c1805fd5214f49678e1
Justin R. Zweifel <jzweifel15@yahoo.com>
2020-05-27 17:04:57 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -6,6 +6,12 @@ class MP3Importer
     @file_path = file_path
   end
   
-  
+    def files
+    @files ||= Dir.entries(@path).select {|song| !File.directory?(song) && song.end_with?(".mp3")}
+  end
+
+  def import
+    files.each {|song| Song.new_by_filename(song)}
+  end
   
 end
