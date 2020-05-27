2e9ed86b6336a935bb3b420533df63ccb31acf1f
Justin R. Zweifel <jzweifel15@yahoo.com>
2020-05-27 17:09:14 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -8,7 +8,7 @@ class MP3Importer
   
   def files
     files = []
-    Dir.new(self.file_path).each do |file|
+    Dir.new(self.path).each do |file|
       files.push(file) if file.length > 4
     end
     return files
